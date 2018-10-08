43493fd9868c6dd6937b819a4a0a0eadcd46eade
Jeeken Wang <i@wangziq.in>
2018-10-09 03:22:49 +0800

    Add range support

--- handler.py
+++ handler.py
@@ -1,3 +1,4 @@
+import logging
 import mimetypes
 import os
 
@@ -25,13 +26,14 @@ class DirBrowseHandler(HandlerBase):
         return super().filtering(request) and os.path.isdir('.' + request.path)
 
     def handle(self, request):
-        doc = page_render.render_dir(request.path)
         mime_type = 'text/html; charset=utf-8'
         if request.method == 'GET':
+            doc = page_render.render_dir(request.path)
             resp = web.HttpResponse(status=200, mimetype=mime_type)
-            resp.add_body(doc)
+            resp.headers['Content-Length'] = len(doc)
+            resp.body = doc
         else:  # request.method == 'HEAD':
-            resp = web.HttpResponse(status=200, mimetype=mime_type, length=len(doc.encode()))
+            resp = web.HttpResponse(status=200, mimetype=mime_type)
         return resp
 
 
@@ -43,19 +45,62 @@ class FileTransHandler(HandlerBase):
         return super().filtering(request) and os.path.isfile('.' + request.path)
 
     def handle(self, request):
-        mime_type = mimetypes.guess_type(request.path, strict=False)[0]
-        if not mime_type:
-            mime_type = 'application/octet-stream'
+        mime = mimetypes.guess_type(request.path)[0] or 'application/octet-stream'
         if request.method == 'GET':
-            resp = web.HttpResponse(status=200, mimetype=mime_type)
+            resp = web.HttpResponse(status=200, mimetype=mime)
             with open('.' + request.path, 'rb') as f:
-                resp.add_body(f.read())
+                file_content = f.read()
+            resp.headers['Content-Length'] = len(file_content)
+            resp.body = file_content
         else:  # request.method = 'HEAD'
-            resp = web.HttpResponse(status=200, mimetype=mime_type, length=os.path.getsize('.' + request.path))
+            resp = web.HttpResponse(status=200, mimetype=mime)
+            resp.headers['Content-Length'] = os.path.getsize('.' + request.path)
+
+        return resp
+
+
+class FileRangeTransHandler(HandlerBase):
+    def __init__(self):
+        super().__init__(methods=('GET',), is_dedicated=True)
+
+    def filtering(self, request) -> bool:
+        return super().filtering(request) and \
+               'Range' in request.headers and \
+               request.headers['Range'].startswith('bytes=') and \
+               os.path.isfile('.' + request.path)
+
+    def handle(self, request):
+        def parse_range(r):
+            range_start, range_end = r.split('-')
+            assert range_start != ''
+            return int(range_start), (int(range_end) if range_end else None)
+        try:
+            ranges = [parse_range(r) for r in request.headers['Range'].lstrip('bytes=').split(', ')]
+        except (ValueError, AssertionError) as e:
+            raise web.ParsingError from e
+
+        if len(ranges) > 1:
+            logging.error('multipart/byteranges requests handling not implemented, returning complete resource')
+            return FileTransHandler().handle(request)
+        relative_path = '.' + request.path
+        file_size = os.path.getsize(relative_path)
+        begin, end = ranges[0]
+        end = end or file_size - 1
+        if not begin <= end < file_size:
+            raise web.RangeNotSatisfiableError
+        content_length = end - begin + 1
+        with open(relative_path, 'rb') as f:
+            f.seek(begin)
+            file_content = f.read(content_length)
+        mime = mimetypes.guess_type(request.path)[0] or 'application/octet-stream'
+        resp = web.HttpResponse(status=206, mimetype=mime)
+        resp.headers['Content-Range'] = 'bytes {}-{}/{}'.format(begin, end, file_size)
+        resp.body = file_content
         return resp
 
 
 def handle_err(status: int, message: str=None):
     resp = web.HttpResponse(status=status, mimetype='text/html; charset=utf-8')
-    resp.add_body(page_render.render_err(status, message))
+    resp.body = page_render.render_err(status, message)
+    resp.headers['Content-Length'] = len(resp.body)
     return resp
--- web.py
+++ web.py
@@ -9,11 +9,27 @@ import urllib.parse
 import handler
 
 
+class ParsingError(RuntimeError):
+    pass
+
+
+class MethodNotAllowedError(ValueError):
+    def __init__(self, method_name: str):
+        self.method_name = method_name
+
+    def __str__(self):
+        return 'HTTP method {} is not allowed'.format(self.method_name)
+
+
+class RangeNotSatisfiableError(ValueError):
+    pass
+
+
 class HttpServer:
     def __init__(self, host: str, port: int = 80):
         self.host = host
         self.port = port
-        self.handlers = [handler.DirBrowseHandler(), handler.FileTransHandler()]
+        self.handlers = [handler.FileRangeTransHandler(), handler.DirBrowseHandler(), handler.FileTransHandler()]
 
     def run(self):
         loop = asyncio.get_event_loop()
@@ -35,7 +51,7 @@ class HttpServer:
             header_data = await reader.readuntil(separator=b'\r\n\r\n')
             request = HttpRequest(header_data)
             if request.method not in ('GET', 'HEAD'):
-                raise HttpServer.MethodNotAllowedError(request.method)
+                raise MethodNotAllowedError(request.method)
             if not os.path.exists('.' + request.path):
                 raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), request.path)
             for hdl in self.handlers:
@@ -43,7 +59,9 @@ class HttpServer:
                     response = hdl.handle(request)
                     break
             logging.info('Sending response: %s', request.path)
-        except HttpRequest.ParsingError:
+        except asyncio.IncompleteReadError:
+            pass
+        except ParsingError:
             logging.warning('Bad request')
             response = handler.handle_err(400)
         except PermissionError as e:
@@ -52,27 +70,23 @@ class HttpServer:
         except FileNotFoundError as e:
            logging.warning('File not exist: %s', e.filename)
            response = handler.handle_err(404)
-        except HttpServer.MethodNotAllowedError as e:
+        except MethodNotAllowedError as e:
             logging.warning('Method not allowed: %s', e.method_name)
             response = handler.handle_err(405)
-        except asyncio.IncompleteReadError:
-            pass
+        except RangeNotSatisfiableError:
+            logging.warning('Range not satisfiable')
+            response = handler.handle_err(416)
         except RuntimeError:
             logging.exception("Oops!")
             response = handler.handle_err(500)
-        finally:
+        try:
             writer.write(response.encode())
             await writer.drain()
             writer.close()
             if sys.version_info >= (3, 7):
                 await writer.wait_closed()
-
-    class MethodNotAllowedError(ValueError):
-        def __init__(self, method_name: str):
-            self.method_name = method_name
-
-        def __str__(self):
-            return 'HTTP method {} is not allowed'.format(self.method_name)
+        except ConnectionError:
+            logging.info('Connection reset/aborted by peer')
 
 
 class HttpRequest:
@@ -87,50 +101,50 @@ class HttpRequest:
                     field_name, field_value = field.split(': ')
                     self.headers[field_name] = field_value
         except ValueError as e:
-            raise HttpRequest.ParsingError('failed to parse the HTTP request') from e
-
-    class ParsingError(RuntimeError):
-        pass
+            raise ParsingError('failed to parse the HTTP request') from e
 
 
 class HttpResponse:
-    PROTOCOL = 'HTTP/1.0'
+    PROTOCOL = 'HTTP/1.1'
 
     STATUS = {
         200: 'OK',
+        206: 'Partial Content',
         301: 'Moved Permanently',
         400: 'Bad Request',
         404: 'Not Found',
         405: 'Method Not Allowed',
+        416: 'Range Not Satisfiable',
         500: 'Internal Server Error'
     }
 
-    def __init__(self, status: int, mimetype: str, length: int = None):
+    def __init__(self, status: int, mimetype: str):
         self.status = status
-        self.mimetype = mimetype
-        self.length = length
-        self.body = None
+        self.headers = {
+            'Connection': 'close',
+            'Content-Type': mimetype,
+            'Accept-Ranges': 'bytes',
+            'Server': 'WebFileBrowser/2.0'
+        }
+        self._body = b''
 
-    def add_body(self, body):
+    @property
+    def body(self):
+        return self._body
+
+    @body.setter
+    def body(self, body):
         body_type = type(body)
         if body_type is bytes:
-            self.body = body
+            self._body = body
         elif body_type is str:
-            self.body = body.encode()
+            self._body = body.encode()
         else:
             raise TypeError('response body should be bytes or string')
-        self.length = len(self.body)
 
     def encode(self) -> bytes:
-        response_head = list()
-        response_head.append('{} {} {}\r\n'.format(HttpResponse.PROTOCOL, self.status, HttpResponse.STATUS[self.status]))
-        fields = {
-            'Connection': 'close',
-            'Server': 'WebFileBrowser/1.0',
-            'Date': time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime()),
-            'Content-Type': self.mimetype,
-            'Content-Length': self.length
-        }
-        response_head.extend(('{}: {}\r\n'.format(name, value) for name, value in fields.items()))
-        response_head_bytes = ''.join(response_head).encode()
-        return b'\r\n'.join([response_head_bytes, self.body if self.body else b''])
+        self.headers['Date'] = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime())
+        response_head = ['{} {} {}'.format(HttpResponse.PROTOCOL, self.status, HttpResponse.STATUS[self.status])]
+        response_head.extend(('{}: {}'.format(name, value) for name, value in self.headers.items()))
+        response_head.append('\r\n')
+        return '\r\n'.join(response_head).encode() + self._body
